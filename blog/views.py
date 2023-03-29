1c73c7ea2da35bad206d72cf19bece2617273304
Min Khati <meetup.min@gmail.com>
2023-03-29 20:52:34 +0000

    Finish Caching and fragment Caching

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug("Got %d posts", len(posts))
