4f9339b273abad86a2f63da810b2c6200b09cdad
Min Khati <meetup.min@gmail.com>
2023-03-31 23:11:01 +0000

    Finish custom User Model

--- blango/settings.py
+++ blango/settings.py
@@ -53,6 +53,7 @@ class Dev(Configuration):
         'crispy_forms',
         'crispy_bootstrap5',
         "debug_toolbar",
+        "blango_auth",
     ]
 
     MIDDLEWARE = [
@@ -98,6 +99,8 @@ class Dev(Configuration):
         ),
     }
 
+    AUTH_USER_MODEL = "blango_auth.User"
+    
     # Password validation
     # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
