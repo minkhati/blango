cc03411de24bf8bd666b21866d38b64c1c375195
Min Khati <meetup.min@gmail.com>
2023-04-03 20:34:38 +0000

    Finish Authentication

--- blango/settings.py
+++ blango/settings.py
@@ -68,7 +68,16 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        "rest_framework.authtoken",
     ]
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
