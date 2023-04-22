d65b7c9d41d9ba9381e80c0f1a3025244e931456
Min Khati <meetup.min@gmail.com>
2023-04-22 21:57:38 +0000

    Finish pagination and django filters

--- blango/settings.py
+++ blango/settings.py
@@ -70,6 +70,7 @@ class Dev(Configuration):
         "rest_framework",
         "rest_framework.authtoken",
         "drf_yasg",
+        "django_filters", 
     ]
 
     REST_FRAMEWORK = {
@@ -92,7 +93,14 @@ class Dev(Configuration):
             "anon_burst": "10/minute",
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
-        },
+        }, 
+        # existing settings omitted
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
     }
 
     MIDDLEWARE = [
