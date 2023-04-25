ec8b079b0b2fee708b0a80c2bf16681eaadb41d0
Min Khati <meetup.min@gmail.com>
2023-04-25 22:28:50 +0000

    Finish fetch and React Hooks

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.urls import reverse
 from blog.forms import CommentForm
 from blog.models import Post
 import logging
@@ -45,4 +46,6 @@ def get_ip(request):
   return HttpResponse(request.META['REMOTE_ADDR'])
 
 def post_table(request):
-  return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
